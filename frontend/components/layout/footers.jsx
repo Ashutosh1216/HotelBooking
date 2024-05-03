61146d6d449412260b330b9356c83d474b793be1
Ashutosh1216 <tiwaryashutosh127@gmail.com>
2024-05-03 19:38:35 +0530

    first commit

---
+++ frontend/components/layout/footers.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Footers() {
+  return (
+    <footer className='footer'>
+      <h2>Beach Resort | Hotel Room Booking System</h2>
+      <p>
+        ©2024 Beach Resort — Developed By
+        {' '}
+        <a
+          href='https://www.linkedin.com/in/ashutosh-tiwary-35a9b9212'
+          rel='noreferrer'
+          target='_blank'
+        >
+          Ashutosh Tiwary
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footers;
